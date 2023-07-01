6ca0b0270c1dc05c0ab3d3d9291cc9845b8c19fb
Rakesh Kumar <cvceup@gmail.com>
2023-07-01 11:42:08 +0530

    init

--- user.php
+++ user.php
@@ -4,10 +4,5 @@
     $data = $userController->getUser();
     if(isset($_POST['save_student']))
     {
-        $fullname = $_POST['fullname'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $class = $_POST['class'];
-
         $userController->saveUser($_POST);
     }
